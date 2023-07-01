825a0326e11dacc0f50938db66a79d01b355232d
Yoshiro0805 <112266312+Yoshiro0805@users.noreply.github.com>
2023-07-01 15:15:12 +0800

    Update and rename build.sh to install.sh

---
+++ install.sh
@@ -0,0 +1,18 @@
+RED="\e[31m"
+GREEN="\e[32m"
+ENDCOLOR="\e[0m"
+
+clear
+echo -e "${GREEN}Installing Proxy...${ENDCOLOR}"
+sleep 1
+if [ -f "proxy" ]; then
+    echo -e "${RED}Deleting old proxy...${ENDCOLOR}"
+    rm proxy_linux
+    sleep 1
+    echo -e "${GREEN}Getting proxy...${ENDCOLOR}"
+fi
+wget -q 
+sleep 1
+echo -e "${GREEN}Proxy Installed${ENDCOLOR}"
+echo -e "${GREEN}Execute proxy with this command: ./proxy ${ENDCOLOR}"
+chmod +x proxy_linux
